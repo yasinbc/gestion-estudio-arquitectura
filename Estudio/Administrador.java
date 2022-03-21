aeb7cb8cae068d3fa31ce7fa3aef27e70ef11c1e
yas <yasyasinbc@gmail.com>
2022-03-21 23:13:31 +0100

    Introduce comentarios que documentan el codigo

--- Estudio/Administrador.java
+++ Estudio/Administrador.java
@@ -10,100 +10,98 @@ import java.io.*;
  */
 public class Administrador
 {
-    // instance variables - replace the example below with your own
-    public ArrayList<String> usuarios;
-    public ArrayList<String> almacenUsuarios;
-    
+    public ArrayList<String> usuarios; //Contiene todos lo registros de los usuarios   
 
     /**
-     * Constructor for objects of class Administrador
+     * Constructor que inizializa las variables
      */
     public Administrador()
     {
-        // initialise instance variables
-        usuarios = new ArrayList<String>(); //esto tiene que ser una ArrayList
-        almacenUsuarios = new ArrayList<String>();
+        usuarios = new ArrayList<String>();
     }
     
+    //añade los usuarios dentro de la ArrayList
     private void guardarUsuarios(String usuario){
         usuarios.add(usuario);
     }
     
+    //devuelve el número de usuarios que hay en la lista
     private int numeroUsuarios(){
         return usuarios.size();
     }
     
+    //imprime en consola usuarios registrados
     public void mostrarUsuarios(){
-        if(numeroUsuarios()<1){
+        if(numeroUsuarios()<1){//si no hay usuarios registrados lanza un mensaje notificandolo
             System.out.println("Todavía no hay usuarios Registrados en el sistema");
-        }else{
+        }else{//Lista a los usuarios y los ordena con un indice
             System.out.println("Lista de usuarios: ");   
             System.out.println("ID         Nombre Usuarios");
             for(String index:usuarios){
-                System.out.println(usuarios.indexOf(index) +"          "+ index);
+                System.out.println(usuarios.indexOf(index) +"          "+ index);//asigna un indice a cada usuario por orden de registro
             }
         }
     }
     
+    //elimina de la lista los usuarios
     public void eliminarUsuario(){
-        String opcion;
-        int opcionInt;
+        String opcion = "";//variable en la que se almacena la opcion en forma de cadena
+        int opcionInt = 0;//variable en la que se almacena la opcion en forma de entero
         
         System.out.println("Usuarios dados de Alta en el sistema: ");
-        mostrarUsuarios();
+        mostrarUsuarios();//muestra los usuarios por consola
         
+        //almacena el la opcion en forma de cadena
         opcion = JOptionPane.showInputDialog("Lista de usuarios en la consola. \nIntroduzca el número del usuario que quiere dar de baja:");
-        opcionInt = Integer.parseInt(opcion);//pasa de string a entero para que lo lea la condicion
+        opcionInt = Integer.parseInt(opcion);//convierte y almacena la opcion de cadena a entero
         
-        for(String index:usuarios){
-            
-        }
+        //si no introduce indice correcto lo notifica para que se repita la secuencia
         if(opcionInt<0 || opcionInt>numeroUsuarios()-1){
             System.out.println("Tiene que introducir un número de usuario entre 0 y "+(numeroUsuarios()-1));
             System.out.println("Intentelo de nuevo, por favor");
-        }else{
+        }else{//remueve usuario seleccionado de la lista
             usuarios.remove(opcionInt);
                
             System.out.println("");
             System.out.println("");
             System.out.println("Ha eliminado al usuarío número "+opcion+" con éxito de la lista anterior.");
-            if(numeroUsuarios() == 0){
+            if(numeroUsuarios() == 0){//si la lista esta vacia lo notifica
                 System.out.println("No hay usuarios registrados en el sistema.");
-            }else{
+            }else{//muestra usuarios restantes en la lista
                 System.out.println("Lista de suarios restantes en el sistema: ");
                 mostrarUsuarios();
             }
         }
-        
     }
     
+    //introduce nuevos usuarios a la lista
     public void altaUsuario(){
-        String opcion; //= JOptionPane.showInputDialog("Opcion dar de ALTA nuevo usuario: ");
-        //int opcionGestion = Integer.parseInt(opcion);//pasa de string a entero para que lo lea la condicion
-        //System.out.println("Ha elegido altas.");
-        opcion = JOptionPane.showInputDialog("Introduzca nombre de usuario a registar: ");
-        guardarUsuarios(opcion);
-        System.out.println(opcion+" dado de alta en el sistema");
+        String opcion = "";//variable que almacenará al usuario en la lista
+        opcion = JOptionPane.showInputDialog("Introduzca nombre de usuario a registar: ");//introduce y almacena opcion
+        guardarUsuarios(opcion);//introduce nuevo usuario en la lista
+        System.out.println(opcion+" dado de alta en el sistema");//muestra en consola nuevo usuario ingresado
     }
     
+    //Reemplaza un nombre por otro en la lista de usuarios
     public void modificarUsuario(){ 
-        String opcion = "";
-        int opcionInt = 0;
-        String nuevoUsuario = "";
+        String opcion = "";//almacenará el nombre del usuario
+        int opcionInt = 0;//álmacenará el indice del usuario
+        String nuevoUsuario = "";//almacenará la conversion del indice del usuario de cadena a entero
         
         System.out.println("Qué usuario desea modificar?");
-        mostrarUsuarios();
+        mostrarUsuarios();//muestra los usuarios por pantalla
         
+        //introduce y almacena el indice del usuario en forma de cadena
         opcion = JOptionPane.showInputDialog("Lista de usuarios en la consola. \nIntroduzca el número del usuario que quiere modificar:");
         opcionInt = Integer.parseInt(opcion);//pasa de string a entero para que lo lea la condicion
         
+        //introduce el numero de usuario el cual modificará
         nuevoUsuario = JOptionPane.showInputDialog("Introduzca el nuevo nombre de usuario: ");
         
-        usuarios.set(opcionInt, nuevoUsuario);
+        //cambia el usuario que estaba por el nuevo introducido
+        usuarios.set(opcionInt, nuevoUsuario);//primer parametro introduce posicion del usuario, segundo parametro introduce nuevo usuario
         
         System.out.println("");
-        mostrarUsuarios();
+        mostrarUsuarios();//muestra los usuarios actuales, incluido el que se ha modificado
     }
-    
-    
 }
